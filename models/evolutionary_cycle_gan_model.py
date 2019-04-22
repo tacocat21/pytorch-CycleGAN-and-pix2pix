a6363d9e519d535b4265b075203b481eb6f30808
Taccio Yamamoto <tyamamo2@illinois.edu>
2019-04-22 14:38:49 -0500

    adding mutation costs

--- models/evolutionary_cycle_gan_model.py
+++ models/evolutionary_cycle_gan_model.py
@@ -15,6 +15,38 @@ class CycleGANModel(BaseModel):
         self.netD_A  = None
         self.netD_B = None
 
+   def backward_G(self, real_A, real_B):
+       pass
+
+# TODO: check if these mutation costs are correct
+# Assuming p_z is uniform distribution
+def minimax_mutation_cost(fake_disc_pred):
+    """
+    Assuming p_z is uniform distribution
+    :param fake_disc_pred: tensor of shape (N). Results of D(G(x))
+    :return: 1/2 * E[log(1- fake_disc_pred)]
+    """
+    log_dist = torch.log(torch.ones(fake_disc_pred.shape[0]) - fake_disc_pred)
+    return -0.5 * log_dist.mean()
+
+def heuristic_mutation_cost(fake_disc_pred):
+    """
+    Assuming p_z is uniform distribution
+    :param fake_disc_pred: tensor of shape (N). Results of D(G(x))
+    :return: -1/2 * E[log(fake_disc_pred)]
+    """
+    log_dist = torch.log(fake_disc_pred)
+    return -0.5 * log_dist.mean()
+
+def least_square_mutation_cost(fake_disc_pred):
+    """
+    Assuming p_z is uniform distribution
+    :param fake_disc_pred: tensor of shape (N). Results of D(G(x))
+    :return: E[(fake_disc_pred - 1)^2]
+    """
+
+    sq_dist = (fake_disc_pred - torch.ones(fake_disc_pred.shape[0]))**2
+    return sq_dist.mean()
 
 class GeneratorPair:
 
@@ -45,3 +77,7 @@ class GeneratorPair:
         self.netG_A = torch.load(os.path.join(self.save_dir, 'netG_A.model'))
         self.netG_B = torch.load(os.path.join(self.save_dir, 'netG_B.model'))
 
+        
+
+if __name__ == '__main__':
+    pass
