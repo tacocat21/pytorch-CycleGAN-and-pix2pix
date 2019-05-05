1a9948615c37e3d5858ebe7f07409f4dfa407d62
Taccio Yamamoto <tyamamo2@illinois.edu>
2019-05-05 15:41:42 -0500

    changed grid size to 6

--- models/evolutionary_cycle_gan_model.py
+++ models/evolutionary_cycle_gan_model.py
@@ -326,11 +326,11 @@ class EvolutionaryCycleGANModel(BaseModel):
         else:
             if not os.path.exists(os.path.join(self.save_dir, self.train_outputs_dir)):
                 os.makedirs(os.path.join(self.save_dir, self.train_outputs_dir))
-            vutils.save_image(fake_A_display, os.path.join(self.save_dir, self.train_outputs_dir, '{}_fake_A.png'.format(epoch)), normalize=True, nrow=8)
-            vutils.save_image(fake_B_display, os.path.join(self.save_dir, self.train_outputs_dir, '{}_fake_B.png'.format(epoch)), normalize=True, nrow=8)
+            vutils.save_image(fake_A_display, os.path.join(self.save_dir, self.train_outputs_dir, '{}_fake_A.png'.format(epoch)), normalize=True, nrow=6)
+            vutils.save_image(fake_B_display, os.path.join(self.save_dir, self.train_outputs_dir, '{}_fake_B.png'.format(epoch)), normalize=True, nrow=6)
 
-            vutils.save_image(self.real_A, os.path.join(self.save_dir, self.train_outputs_dir, '{}_real_A.png'.format(epoch)), normalize=True, nrow=8)
-            vutils.save_image(self.real_B, os.path.join(self.save_dir, self.train_outputs_dir, '{}_real_B.png'.format(epoch)), normalize=True, nrow=8)
+            vutils.save_image(self.real_A, os.path.join(self.save_dir, self.train_outputs_dir, '{}_real_A.png'.format(epoch)), normalize=True, nrow=6)
+            vutils.save_image(self.real_B, os.path.join(self.save_dir, self.train_outputs_dir, '{}_real_B.png'.format(epoch)), normalize=True, nrow=6)
 
 
 # Assuming p_z is uniform distribution
